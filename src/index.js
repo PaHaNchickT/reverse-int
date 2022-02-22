6d4080c9196297857147b7a34dfe9b04c84c3c7a
Pavel <you@example.com>
2022-02-22 21:16:08 +0300

    feat: done exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let out = ''
+    if (n<0) {
+        n = -n
+    }
+    n = String(n).split('')
+    let index = n.length-1
+    while (index>-1) {
+        out=`${out}${n[index]}`
+        index = index-1
+    }
+    return(Number(out))
 }
+
+// reverse(-425)
